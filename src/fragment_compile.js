d926751d2581be79794f109a8d6a23f0f3d074d0
yskszk63 <yskszk63@gmail.com>
2021-09-23 14:02:11 +0900

    fmt

--- src/fragment_compile.js
+++ src/fragment_compile.js
@@ -1,25 +1,32 @@
 export function compile() {
-  if (typeof ReadableByteStreamController !== 'undefined' && typeof WebAssembly.compileStreaming === 'function') {
+  if (
+    typeof ReadableByteStreamController !== "undefined" &&
+    typeof WebAssembly.compileStreaming === "function"
+  ) {
     const iter = data();
     const stream = new ReadableStream({
       pull(controller) {
         const result = iter.next();
         if (result.done) {
-            controller.close();
+          controller.close();
         }
-        controller.enqueue(Uint8Array.from(atob(result.value), c => c.charCodeAt(0)));
+        controller.enqueue(
+          Uint8Array.from(atob(result.value), (c) => c.charCodeAt(0)),
+        );
       },
-      type: 'bytes',
+      type: "bytes",
     });
-    return WebAssembly.compileStreaming(new Response(stream, {
-      headers: [
-        ['Content-Type', 'application/wasm'],
-      ],
-    }));
+    return WebAssembly.compileStreaming(
+      new Response(stream, {
+        headers: [
+          ["Content-Type", "application/wasm"],
+        ],
+      }),
+    );
   } else {
     const content = [];
     for (const d of data()) {
-      content.push(...Array.from(atob(d), c => c.charCodeAt(0)));
+      content.push(...Array.from(atob(d), (c) => c.charCodeAt(0)));
     }
     return WebAssembly.compile(Uint8Array.from(content));
   }
--- src/wasmto-js.ts
+++ src/wasmto-js.ts
@@ -1,12 +1,19 @@
-import {Base64TransformStream, IntoGeneratorCodeTransformStream, AppendTrailerTransformStream} from "./stream";
-import {compile} from "../gen/fragments";
+import {
+  AppendTrailerTransformStream,
+  Base64TransformStream,
+  IntoGeneratorCodeTransformStream,
+} from "./stream";
+import { compile } from "../gen/fragments";
 
-export async function wasmtoJs(input: ReadableStream<Uint8Array>, output: WritableStream<Uint8Array>) {
-    // TODO check header
-    await input
-        .pipeThrough(new Base64TransformStream())
-        .pipeThrough(new IntoGeneratorCodeTransformStream())
-        .pipeThrough(new AppendTrailerTransformStream(compile))
-        .pipeThrough(new TextEncoderStream())
-        .pipeTo(output);
+export async function wasmtoJs(
+  input: ReadableStream<Uint8Array>,
+  output: WritableStream<Uint8Array>,
+) {
+  // TODO check header
+  await input
+    .pipeThrough(new Base64TransformStream())
+    .pipeThrough(new IntoGeneratorCodeTransformStream())
+    .pipeThrough(new AppendTrailerTransformStream(compile))
+    .pipeThrough(new TextEncoderStream())
+    .pipeTo(output);
 }
